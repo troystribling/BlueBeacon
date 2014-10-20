2c23010ebed556cd21f530421830fab385720dbe
Troy Stribling <troy.stribling@gmail.com>
2014-10-20 08:35:12 -0700

    created project

---
+++ BlueBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueBeacon
+//
+//  Created by Troy Stribling on 10/20/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
